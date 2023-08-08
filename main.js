bcede5e4f7cdf5ebe9b5336ce2e8b96926062bf9
jeevunSagoo <130655721+jeevunSagoo@users.noreply.github.com>
2023-08-08 11:14:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 var canvas = new fabric.Canvas('myCanvas');
- var x= document.getElementById("myAudio");
+var x = document.getElementById("myAudio");
 
-function new_image()
-{
-	fabric.Image.fromURL("BirthdayImage.jpg", function(img)) {
-        block_image_object = Img;
+function new_image() {
+    fabric.Image.fromURL("BirthdayImage.jpg", function (img) {
+        block_image_object = img;
 
         block_image_object.scaleToWidth(700);
-        block_image_object.scaleToheight(510);
+        block_image_object.scaleToHeight(510);
         block_image_object.set({
-        top:0,
-        left:0    
+            top: 0,
+            left: 0
         });
         canvas.add(block_image_object);
-    }
-	
+    });
+
 }
 
-function playSound(){
-	x.play();
+function playSound() {
+    x.play();
 }
